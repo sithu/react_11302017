c8dadc2e7be2317601e6779ce574772dfd20409f
Sithu Aung <sithu_aung@intuit.com>
2017-12-01 10:27:22 -0800

    add constant headers in CardTable

--- src/components/CarTable.js
+++ src/components/CarTable.js
@@ -2,11 +2,13 @@ import * as React from 'react';
 
 export class CarTable extends React.Component {
     render() {
+        const headers = [ 'Make', 'Model', 'Year', 'Color', 'Price' ];
         return (
             <table>
                 <tbody>
                 <tr>
-                    {Object.keys(this.props.cars[0]).map(key => <th>{key}</th>)}
+                    {/* {Object.keys(this.props.cars[0]).map(key => <th>{key}</th>)} */}
+                    {headers.map(header => <th>{header}</th>)}
                 </tr>
                 {this.props.cars.map(car => 
                     <tr>
